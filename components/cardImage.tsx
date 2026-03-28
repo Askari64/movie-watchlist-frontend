ec5463dae09050f6084fac20c27bdec693ff2b56
Askari <askarir64@gmail.com>
2026-03-28 07:13:39 +0530

    cardImage WIP

---
+++ components/cardImage.tsx
@@ -0,0 +1,38 @@
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import Image from "next/image"
+
+export function CardImage() {
+  return (
+    <Card className="relative mx-auto w-full max-w-xs pt-0">
+      <Image
+        src="https://avatar.vercel.sh/shadcn1"
+        alt="Event cover"
+        width="384"
+        height="216"
+        className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+      />
+      <CardHeader>
+        <CardAction>
+          <Badge variant="secondary">Featured</Badge>
+        </CardAction>
+        <CardTitle>Design systems meetup</CardTitle>
+        <CardDescription>
+          A practical talk on component APIs, accessibility, and shipping
+          faster.
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <Button className="w-full">View Event</Button>
+      </CardFooter>
+    </Card>
+  )
+}
