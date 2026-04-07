be3748987db4bf654fe14d15d080e8fda984c3ef
Askari <askarir64@gmail.com>
2026-04-07 22:58:07 +0530

    proxy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/movies/:path*",
+        destination: "http://127.0.0.1:5000/movies/:path*",
+      },
+      {
+        source: "/auth/:path*",
+        destination: "http://127.0.0.1:5000/auth/:path*",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
@@ -8,15 +20,15 @@ const nextConfig: NextConfig = {
         hostname: "avatar.vercel.sh",
       },
       {
-      protocol: "https",
-      hostname: "media.themoviedb.org",
-      pathname: "/t/p/**",
-    },
-    {
-      protocol: "https",
-      hostname: "image.tmdb.org",
-      pathname: "/t/p/**",
-    },
+        protocol: "https",
+        hostname: "media.themoviedb.org",
+        pathname: "/t/p/**",
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
     ],
   },
 };
