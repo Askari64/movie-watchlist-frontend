e0c77c29f1501ccb7b8c1fdd43ce493d72579609
Askari <askarir64@gmail.com>
2026-04-10 19:13:48 +0530

    added props and types
    removed irrelevant fields

--- components/cardImage.tsx
+++ components/cardImage.tsx
@@ -1,34 +1,29 @@
-import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import {
   Card,
-  CardAction,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import Image from "next/image"
 
-export function CardImage() {
+type CardImageProps = {
+  title: string;
+  imageUrl: string;
+}
+
+export function CardImage({title, imageUrl}: CardImageProps) {
   return (
-    <Card className="relative mx-auto w-full max-w-xs pt-0">
+    <Card className="relative mx-auto max-w-xs pt-0">
       <Image
-        src="https://avatar.vercel.sh/shadcn1"
-        alt="Event cover"
+        src={imageUrl}
+        alt={title}
         width="384"
         height="216"
-        className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+        className="relative z-20 aspect-video w-full object-cover brightness-60 dark:brightness-40"
       />
       <CardHeader>
-        <CardAction>
-          <Badge variant="secondary">Featured</Badge>
-        </CardAction>
-        <CardTitle>Design systems meetup</CardTitle>
-        <CardDescription>
-          A practical talk on component APIs, accessibility, and shipping
-          faster.
-        </CardDescription>
+        <CardTitle>{title}</CardTitle>
       </CardHeader>
       <CardFooter>
         <Button className="w-full">View Event</Button>
