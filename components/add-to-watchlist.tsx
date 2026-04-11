aab705be7d90c5aacca40a1d1c519ffa7de66199
Askari <askarir64@gmail.com>
2026-04-11 16:44:14 +0530

    WIP add to watchlist component

---
+++ components/add-to-watchlist.tsx
@@ -0,0 +1,51 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  BookmarkPlus,
+  CheckCircle,
+  Clock,
+  PlayCircle,
+  StopCircle,
+} from "lucide-react";
+
+function AddToWatchlist() {
+  const handleAddToWatchlist = (status: string) => {
+    console.log(status);
+  };
+
+  return (
+    <div className="absolute top-2 right-2 z-30">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button className="p-2 rounded-full bg-black/50 hover:bg-black/70 backdrop-blur-sm transition-all">
+            <BookmarkPlus className="h-3 w-3 text-white" />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuItem onClick={() => handleAddToWatchlist("PLANNED")}>
+            <Clock className="mr-2 h-4 w-4" />
+            <span>Plan to Watch</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => handleAddToWatchlist("WATCHING")}>
+            <PlayCircle className="mr-2 h-4 w-4" />
+            <span>Watching</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => handleAddToWatchlist("COMPLETED")}>
+            <CheckCircle className="mr-2 h-4 w-4" />
+            <span>Completed</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => handleAddToWatchlist("DROPPED")}>
+            <StopCircle className="mr-2 h-4 w-4" />
+            <span>Dropped</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
+
+export default AddToWatchlist;
