c4d70c173df8d59467480aa857dd91d5aa955b42
Askari <askarir64@gmail.com>
2026-04-12 15:10:55 +0530

    simplified rewrites

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/movies/:path*",
-        destination: "http://127.0.0.1:5000/movies/:path*",
-      },
-      {
-        source: "/api/auth/:path*",
-        destination: "http://127.0.0.1:5000/auth/:path*",
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:5000/:path*",
       },
     ];
   },
