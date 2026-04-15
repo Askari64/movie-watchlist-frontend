059d88647ca5e86f718ce5a3e09798a3a75ec7ff
Askari <askarir64@gmail.com>
2026-04-16 03:24:50 +0530

    protected routes with proxy/middleware

---
+++ proxy.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// List public routes because easier to track
+const publicRoutes = ["/", "/signin", "/signup"];
+
+export function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  //Skip proxy/middleware for API routes - they pass through backend
+  if (pathname.startsWith("/api/")) {
+    return NextResponse.next();
+  }
+
+  // Check if request is for protected route -  if no then continue to requested route - if yes then check cookie for authentication
+  const isProtectedRoute = !publicRoutes.some(
+    (route) => pathname == route || pathname.startsWith(`${route}/`),
+  );
+
+  const token = request.cookies.get("jwtAccessToken")?.value;
+
+  // if isProtectedRoute and no token then redirect to login page
+  if (isProtectedRoute && !token) {
+    const signInUrl = new URL("/signin", request.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  // if token exists and user is trying to access signin or signup page then redirect to /movies page
+  if (token && (pathname === "/signin" || pathname === "/signup")) {
+    return NextResponse.redirect(new URL("/movies", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// add config matcher to apply middleware to specific routes
+
+/*
+ * Match all request paths except:
+ * - _next/static (static files)
+ * - _next/image (image optimization files)
+ * - favicon.ico (favicon file)
+ * - public folder files
+ */
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
