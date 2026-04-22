acc1368f6832e3a381afb4f0b88faf57e4e92747
Askari <askarir64@gmail.com>
2026-04-22 07:59:13 +0530

    removed server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { CardImage } from "@/components/cardImage";
+
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-       <CardImage title="Joker" imageUrl="https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg" movieId="4532"/>
       </main>
     </div>
   );
